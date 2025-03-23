4cf1d71f06c73f9f1fad6d7fe61cff9b502949b8
SANTHAN-KUMAR <74782855+SANTHAN-KUMAR@users.noreply.github.com>
2025-03-23 12:19:57 +0530

    Update main.py
    updated the list of scripts to be executed

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # List of scripts to run in sequence
 steps = [
+    'setup_and_prepare.py',
+    'split_data.py',
+    'convert_to_tiff.py',
+    'preprocess_images.py',
+    'visualize_comparison.py',
+    'verify_pixels.py',
+    'create_ground_truth.py',
+    'generate_box_files.py',
+    'generate_lstmf_files.py',
+    'create_file_lists.py',
+    'extract_eng_lstm.py',
     'finetune_model.py',
     'finalize_model.py',
-    'test_model.py'
+    'test_model.py',
 ]
 
 if __name__ == "__main__":
